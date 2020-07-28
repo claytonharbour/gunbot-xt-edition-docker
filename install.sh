5e6044f02fa07eb1b20b49f040d85d242a3d090e
Clayton Harbour <claytonharbour@gmail.com>
2020-07-28 13:08:53 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,8 @@ function get_download_version {
 echo $GUNBOT_VERSION | tr -d .
 }
 
-DOWNLOAD_VERSION=$(get_download_version)
+# DOWNLOAD_VERSION=$(get_download_version)
+DOWNLOAD_VERSION=$GUNBOT_VERSION
 
 wget https://github.com/GuntharDeNiro/BTCT/releases/download/v$DOWNLOAD_VERSION/lin.zip
 unzip lin.zip
